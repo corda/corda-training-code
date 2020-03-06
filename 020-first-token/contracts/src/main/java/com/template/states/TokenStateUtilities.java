de6727ee01d85850be82c425f2ddd98f401074cd
Xavier Lepretre <xavier.lepretre@gmail.com>
2020-03-06 22:55:51 +0400

    Made utilities a static interface.

--- 020-first-token/contracts/src/main/java/com/template/states/TokenStateUtilities.java
+++ 020-first-token/contracts/src/main/java/com/template/states/TokenStateUtilities.java
@@ -6,29 +6,29 @@ import net.corda.core.identity.Party;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class TokenStateUtilities {
+public interface TokenStateUtilities {
 
-    private static List<Long> getQuantityAsSingletonList(final TokenState state) {
+    static List<Long> getQuantityAsSingletonList(final TokenState state) {
         return Collections.singletonList(state.getQuantity());
     }
 
-    private static <T> List<T> merge(final List<T> left, final List<T> right) {
+    static <T> List<T> merge(final List<T> left, final List<T> right) {
         final List<T> merged = new ArrayList<>(left);
         merged.addAll(right);
         return merged;
     }
 
-    private static <K> Map.Entry<K, Long> mapSumValues(final Map.Entry<K, List<Long>> entry) {
+    static <K> Map.Entry<K, Long> mapSumValues(final Map.Entry<K, List<Long>> entry) {
         return new HashMap.SimpleEntry<>(
                 entry.getKey(),
                 sumExact(entry.getValue()));
     }
 
-    private static Long sumExact(final List<Long> values) {
+    static Long sumExact(final List<Long> values) {
         return values.stream().reduce(0L, Math::addExact);
     }
 
-    public static Map<Party, Long> mapSumByIssuer(final Collection<TokenState> states) {
+    static Map<Party, Long> mapSumByIssuer(final Collection<TokenState> states) {
         return ImmutableMap.copyOf(states.stream()
                 .collect(Collectors.toConcurrentMap(
                         TokenState::getIssuer,
