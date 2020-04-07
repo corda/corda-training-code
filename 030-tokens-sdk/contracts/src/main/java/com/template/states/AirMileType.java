3a2ea07edc85b567c0955a4ab307c7b7343faab8
Xavier Lepretre <xavier.lepretre@gmail.com>
2020-04-08 02:04:12 +0400

    Added comment on AirMileType.

--- 030-tokens-sdk/contracts/src/main/java/com/template/states/AirMileType.java
+++ 030-tokens-sdk/contracts/src/main/java/com/template/states/AirMileType.java
@@ -2,12 +2,22 @@ package com.template.states;
 
 import com.r3.corda.lib.tokens.contracts.types.TokenType;
 import org.jetbrains.annotations.NotNull;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class AirMileType {
+public final class AirMileType {
 
     public static final String IDENTIFIER = "AIR";
     public static final int FRACTION_DIGITS = 0;
 
+    private AirMileType() {
+        // Do not instantiate
+        throw new NotImplementedException();
+    }
+
+    /**
+     * This creates the {@link TokenType} for air-miles. We do not create instances of {@link AirMileType} as this can
+     * create issues of JAR hash.
+     */
     @NotNull
     public static TokenType create() {
         return new TokenType(IDENTIFIER, FRACTION_DIGITS);
